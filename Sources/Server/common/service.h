55201ff32f10ca83fe4b5755ebcc600322d81481
Owsap <owsap@outlook.com>
2023-05-29 14:55:00 +0000

    Added final damage multiplier.

--- Sources/Server/common/service.h
+++ Sources/Server/common/service.h
@@ -1,3 +1,6 @@
 /// 1.
 // Add
 #define /* @author: Owsap */ __SOUL_SYSTEM__ // Soul System
+#if defined(__SOUL_SYSTEM__)
+#	define __SOUL_SYSTEM_CALC_FINAL_DAMAGE__
+#endif
--- Sources/Server/game/char_battle.cpp
+++ Sources/Server/game/char_battle.cpp
@@ -47,7 +47,7 @@
 				if (pAttacker->GetPoint(POINT_NORMAL_HIT_DAMAGE_BONUS))
 					dam = dam * (100 + pAttacker->GetPoint(POINT_NORMAL_HIT_DAMAGE_BONUS)) / 100;
 
-#if defined(__SOUL_SYSTEM__)
+#if defined(__SOUL_SYSTEM__) && !defined(__SOUL_SYSTEM_CALC_FINAL_DAMAGE__)
 				if (pAttacker->FindAffect(AFFECT_SOUL, AFF_SOUL_RED))
 					dam = dam * (100 + pAttacker->GetSoulDamage(RED_SOUL)) / 100;
 #endif
@@ -91,7 +91,7 @@
 				if (pAttacker->GetPoint(POINT_SKILL_DAMAGE_BONUS))
 					dam = dam * (100 + pAttacker->GetPoint(POINT_SKILL_DAMAGE_BONUS)) / 100;
 
-#if defined(__SOUL_SYSTEM__)
+#if defined(__SOUL_SYSTEM__) && !defined(__SOUL_SYSTEM_CALC_FINAL_DAMAGE__)
 				if (pAttacker->FindAffect(AFFECT_SOUL, AFF_SOUL_BLUE))
 					dam = dam * (100 + pAttacker->GetSoulDamage(BLUE_SOUL)) / 100;
 #endif
@@ -100,3 +100,45 @@
 			dam = dam * (100 - MIN(99, GetPoint(POINT_SKILL_DEFEND_BONUS))) / 100;
 		}
 		break;
+
+/// 5.
+// Search
+		if (IsPC() && CMonarch::instance().IsDefenceUp(GetEmpire()))
+		{
+			// 10% ÇÇÇØ °¨¼Ò
+			dam -= dam / 10;
+		}
+
+// Add below
+#if defined(__SOUL_SYSTEM__) && defined(__SOUL_SYSTEM_CALC_FINAL_DAMAGE__)
+		switch (type)
+		{
+			case EDamageType::DAMAGE_TYPE_NORMAL:
+			case EDamageType::DAMAGE_TYPE_NORMAL_RANGE:
+			{
+				if (pAttacker->FindAffect(AFFECT_SOUL, AFF_SOUL_RED))
+				{
+					float fDamMultiplier =  static_cast<float>(100 * pAttacker->GetSoulDamage(RED_SOUL)) / 1000;
+					if (fDamMultiplier != 0)
+						dam = (int)(dam * fDamMultiplier);
+				}
+			}
+			break;
+
+			case EDamageType::DAMAGE_TYPE_MELEE:
+			case EDamageType::DAMAGE_TYPE_RANGE:
+			case EDamageType::DAMAGE_TYPE_FIRE:
+			case EDamageType::DAMAGE_TYPE_ICE:
+			case EDamageType::DAMAGE_TYPE_ELEC:
+			case EDamageType::DAMAGE_TYPE_MAGIC:
+			{
+				if (pAttacker->FindAffect(AFFECT_SOUL, AFF_SOUL_BLUE))
+				{
+					float fDamMultiplier =  static_cast<float>(100 * pAttacker->GetSoulDamage(BLUE_SOUL)) / 1000;
+					if (fDamMultiplier != 0)
+						dam = (int)(dam * fDamMultiplier);
+				}
+			}
+			break;
+		}
+#endif
